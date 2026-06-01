31a34c8fde9ae7603c91ba5f8c0f222e4aac9800
hurry-woodvil <hurry.woodvil@gmail.com>
2026-06-01 15:34:53 +0900

    feat: configuration schemaを定義 refs #30

--- lua/orbit/config/schema.lua
+++ lua/orbit/config/schema.lua
@@ -1,3 +1,17 @@
 local M = {}
 
+---@class OrbitConfig
+---@field default_provider string
+---@field providers table<string, OrbitProviderConfig>
+---@field workspace OrbitWorkspaceConfig
+
+---@class OrbitProviderConfig
+---@field command string
+---@field args string[]
+
+---@class OrbitWorkspaceConfig
+---@field position "left"|"right"|"top"|"bottom"
+---@field width integer
+---@field height integer
+
 return M
