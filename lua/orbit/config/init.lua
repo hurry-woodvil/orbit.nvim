251a9982b5850401303dcfaa6c5cd52a39b7993e
hurry-woodvil <hurry.woodvil@gmail.com>
2026-06-01 15:44:22 +0900

    feat: user optsとdefault configurationをmerge refs #33

--- lua/orbit/config/init.lua
+++ lua/orbit/config/init.lua
@@ -2,14 +2,21 @@ local defaults = require("orbit.config.defaults")
 
 local M = {}
 
+---@type OrbitConfig
+local current = vim.deepcopy(defaults)
+
+---@param opts OrbitConfig|nil
 function M.setup(opts)
+  current = vim.tbl_deep_extend("force", vim.deepcopy(defaults), opts or {})
 end
 
+---@return OrbitConfig
 function M.get()
-  return vim.deepcopy(defaults)
+  return vim.deepcopy(current)
 end
 
 function M.reset()
+  current = vim.deepcopy(defaults)
 end
 
 return M
